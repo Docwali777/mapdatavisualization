37010cd46ef66c8087b0ec02526789e065681087
drgauvin <drgauvin@mgmcare.org>
2017-01-27 14:14:18 -0500

    trying topojson

--- js.js
+++ js.js
@@ -2,32 +2,25 @@
 let url = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json",
     w= 1000,
     h= 800
-    worldmap = "https://rawgit.com/Docwali777/mapdatavisualization/master/world-map.json";
+    worldmap = "https://rawgit.com/Docwali777/mapdatavisualization/master/world-map.json",
+    mapTopo = "https://rawgit.com/Docwali777/mapdatavisualization/master/ne_110m_oceanTOPO.json";
 
 
 
 
 
 var projection = d3.geoMercator()
-.scale(w  / Math.PI)
-
 var path = d3.geoPath(projection)
 
   var svg = d3.select("body").append("svg").attr("width", w).attr("height", h)
 
-      var projection = d3.geoMercator()
+     d3.json("worldmap.json", function(err, geojson) {
+var topology = topojson.topology({mapTopo: mapTopo})
+topojson.presimplify(topology);
 
-
-      var path = d3.geoPath()
-        .projection(projection);
-
-
-
-
-     d3.json(worldmap, function(err, geojson) {
-
+console.log(geojson)
        svg.append("path")
        .attr("class", "map")
          .attr("d", path(geojson))
-         console.log(geojson.features[0])
+
      })
