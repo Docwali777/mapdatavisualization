f26cf6b7797fda7e6be1910bce692ce6132d6d06
drgauvin <drgauvin@mgmcare.org>
2017-01-27 10:50:37 -0500

    Added topo.json file

--- js.js
+++ js.js
@@ -1,16 +1,19 @@
 
 let url = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json",
-w= 1000,
-h= 800;
+    w= 1000,
+    h= 800
+    worldmap = "https://rawgit.com/Docwali777/mapdatavisualization/master/world-map.json";
+
+
 
 
 
 var projection = d3.geoMercator()
-// .scale(w  / Math.PI)
-//.translate([w / 2, h / 2])
+.scale(w  / Math.PI)
+
 var path = d3.geoPath(projection)
 
-  var svg = d3.select("body").append("svg")
+  var svg = d3.select("body").append("svg").attr("width", w).attr("height", h)
 
       var projection = d3.geoMercator()
 
@@ -18,9 +21,13 @@ var path = d3.geoPath(projection)
       var path = d3.geoPath()
         .projection(projection);
 
-     d3.json(url, function(err, geojson) {
+
+
+
+     d3.json(worldmap, function(err, geojson) {
 
        svg.append("path")
+       .attr("class", "map")
          .attr("d", path(geojson))
          console.log(geojson.features[0])
      })
