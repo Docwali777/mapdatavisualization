69683048cbb3b1c98a062a59ed95c9aa507c7603
Wali Gauvin <drgauvin@mgmcare.org>
2017-01-28 21:26:30 -0500

    Add files via upload

--- js.js
+++ js.js
@@ -1,26 +1,58 @@
 
 let url = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json",
-    w= 1000,
-    h= 800
-    worldmap = "https://rawgit.com/Docwali777/mapdatavisualization/master/world-map.json",
-    mapTopo = "https://rawgit.com/Docwali777/mapdatavisualization/master/ne_110m_oceanTOPO.json";
+    worldfile = "https://rawgit.com/Docwali777/mapdatavisualization/master/world-map.json",
+    h = 1000,
+    w = 1200;
+let projection = d3.geoMercator()
 
 
+let path = d3.geoPath()
+  .projection(projection)
 
+  var svg = d3.select("body").append("svg").attr("height", h).attr("width", w).style("border", "1px solid black")
 
-
-var projection = d3.geoMercator()
-var path = d3.geoPath(projection)
-
-  var svg = d3.select("body").append("svg").attr("width", w).attr("height", h)
-
-     d3.json("worldmap.json", function(err, geojson) {
-var topology = topojson.topology({mapTopo: mapTopo})
-topojson.presimplify(topology);
-
-console.log(geojson)
-       svg.append("path")
-       .attr("class", "map")
-         .attr("d", path(geojson))
-
-     })
+// d3.queue()
+//     .defer(d3.json, url)
+//     .defer(d3.json, worldfile)
+//
+// //Access JSON data for Meteorite-strike-data
+// d3.json(url, function(data){
+//
+//
+// var circle = svg.selectAll("class", "  circle").data(data.features).enter().append("circle")
+//
+// circle.classed("over", true)
+// .attr("cx", function(d){
+// if (d.geometry ===null){return 0}
+// else {return projection(d.geometry.coordinates)[0]}
+//
+// })
+// .attr("cy", function(d){
+// if (d.geometry ===null){return 0}
+// else {return projection(d.geometry.coordinates)[1]}
+//
+// })
+//
+//
+//
+//
+// // console.log(data)
+//
+// })
+//
+// d3.json(worldfile, function(map){
+//
+// svg.selectAll("path")
+// .data(map.features).enter().append("path")
+// .attr("d", path)
+//
+//
+//
+// console.log(map.features[0].properties.sovereignt)
+// })
+d3.json("practiceMap.json", function(map){
+  svg.selectAll("path")
+  .data(map.features).enter().append("path")
+  .attr("d", path)
+  console.log(map)
+})
